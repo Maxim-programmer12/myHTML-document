7f0e281911cb4f7a97eae802cf09c66bb28f20e0
Maxim <samusevicmaksim91@gmail.com>
2026-02-11 15:09:28 +0300

    script.js
    Its script for HTML-document.

---
+++ script.js
@@ -0,0 +1,45 @@
+const btn = document.getElementById("btn");
+const input = document.getElementById("username");
+const welcome = document.getElementById("welcome");
+const counter = document.getElementById("counter");
+const themeBtn = document.getElementById("themeBtn");
+
+let clicks = 0;
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark"); 
+});
+
+btn.addEventListener("click", () => {
+    const name = input.value.trim();
+
+    welcome.classList.remove("show", "fade-out");
+
+    clicks++;
+    counter.textContent = `Нажатий: ${clicks}`;
+
+    if (!name) {
+        welcome.textContent = "Введите имя!";
+        welcome.style.color = "red";
+        welcome.style.display = "block";
+
+        setTimeout(() => welcome.classList.add("show"), 10);
+
+        setTimeout(() => welcome.classList.add("fade-out"), 3000);
+        return;
+    }
+
+    welcome.textContent = `Привет, ${name}!`;
+    welcome.style.color = "blue";
+    welcome.style.display = "block";
+
+    setTimeout(() => welcome.classList.add("show"), 10);
+
+    setTimeout(() => welcome.classList.add("fade-out"), 3000);
+
+    const new_text = document.createElement("p");
+    new_text.textContent = "Этот сайт будет развиваться!";
+    new_text.style.color = "green";
+
+    document.body.appendChild(new_text);
+});
